0f8241a492497a9e04cd312cb357134f83da555d
Karthik <karthikykr16@gmail.com>
2025-03-24 17:49:10 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import http from 'http';
+import app from './app.js';
+
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+
+
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
